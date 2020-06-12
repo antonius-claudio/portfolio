c117ce8a9fd4aca6187ae235203c0a7e360468cc
Claudio Antonius <antonius.claudio@gmail.com>
2020-06-12 16:53:10 +0700

    mobile navbar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
 import HomeIcon from '@material-ui/icons/Home';
 import FaceIcon from '@material-ui/icons/Face';
 import WorkIcon from '@material-ui/icons/Work';
 import FolderIcon from '@material-ui/icons/Folder';
 import ContactMailIcon from '@material-ui/icons/ContactMail';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function NavigationBar () {
+    let [url, setUrl] = React.useState('');
+    let location = useLocation().pathname;
+
+    useEffect(() => {
+        setUrl(location);
+    }, [location])
+
     return (
         <AppBar position="static" className="boxNavbar">
             <Toolbar className="containerNavbar">
@@ -23,56 +28,86 @@ export default function NavigationBar () {
                     </Link>
                     <div className="desktopMenu">
                         <Link to='/'>
-                            <Button color="inherit">Home</Button>
+                        {url === '/' ? 
+                            <Button color="inherit" className='activePage'>Home</Button>
+                        :
+                            <Button color="inherit" >Home</Button>
+                        }
                         </Link>
                         <Link to='/profile'>
-                            <Button color="inherit">Profile</Button>
+                        {url === '/profile' ? 
+                            <Button color="inherit" className='activePage'>Profile</Button>
+                        :
+                            <Button color="inherit" >Profile</Button>
+                        }
                         </Link>
                         <Link to='/experience'>
-                            <Button color="inherit">Experience</Button>
+                        {url === '/experience' ? 
+                            <Button color="inherit" className='activePage'>Experience</Button>
+                        :
+                            <Button color="inherit" >Experience</Button>
+                        }
                         </Link>
                         <Link to='/projects'>
-                            <Button color="inherit">Projects</Button>
+                        {url === '/projects' ? 
+                            <Button color="inherit" className='activePage'>Projects</Button>
+                        :
+                            <Button color="inherit" >Projects</Button>
+                        }
                         </Link>
                         <Link to='/contact'>
-                            <Button color="inherit">Contact</Button>
+                        {url === '/contact' ? 
+                            <Button color="inherit" className='activePage'>Contact</Button>
+                        :
+                            <Button color="inherit" >Contact</Button>
+                        }
                         </Link>
                     </div>
                 <div className="mobileMenu">
                     <Link to='/'>
-                        {/* <MenuItem> */}
-                            <IconButton>
-                                <HomeIcon />
-                            </IconButton>
-                        {/* </MenuItem> */}
+                        <IconButton>
+                        {url === '/' ? 
+                            <HomeIcon className='activePageMobile'/>
+                        :
+                            <HomeIcon />
+                        }
+                        </IconButton>
                     </Link>
                     <Link to='/profile'>
-                        {/* <MenuItem> */}
-                            <IconButton>
-                                <FaceIcon />
-                            </IconButton>
-                        {/* </MenuItem> */}
+                        <IconButton>
+                        {url === '/profile' ? 
+                            <FaceIcon className='activePageMobile'/>
+                        :
+                            <FaceIcon />
+                        }
+                        </IconButton>
                     </Link>
                     <Link to='/experience'>
-                        {/* <MenuItem> */}
-                            <IconButton>
-                                <WorkIcon />
-                            </IconButton>
-                        {/* </MenuItem> */}
+                        <IconButton>
+                        {url === '/experience' ? 
+                            <WorkIcon className='activePageMobile'/>
+                        :
+                            <WorkIcon />
+                        }
+                        </IconButton>
                     </Link>
                     <Link to='/projects'>
-                        {/* <MenuItem> */}
-                            <IconButton>
-                                <FolderIcon />
-                            </IconButton>
-                        {/* </MenuItem> */}
+                        <IconButton>
+                        {url === '/projects' ? 
+                            <FolderIcon className='activePageMobile'/>
+                        :
+                            <FolderIcon />
+                        }
+                        </IconButton>
                     </Link>
                     <Link to='/contact'>
-                        {/* <MenuItem> */}
-                            <IconButton>
-                                <ContactMailIcon />
-                            </IconButton>
-                        {/* </MenuItem> */}
+                        <IconButton>
+                        {url === '/contact' ? 
+                            <ContactMailIcon className='activePageMobile'/>
+                        :
+                            <ContactMailIcon />
+                        }
+                        </IconButton>
                     </Link>
                 </div>
             </Toolbar>
